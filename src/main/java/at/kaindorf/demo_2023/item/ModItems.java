49cc398fa38290350ac8b6bdc3e3b839071ec543
gigpab20 <gigpab20@htl-kaindorf.at>
2024-01-02 22:31:50 +0100

    Changed ItemInit File to ModItems

---
+++ src/main/java/at/kaindorf/demo_2023/item/ModItems.java
@@ -0,0 +1,37 @@
+package at.kaindorf.demo_2023.item;
+
+import at.kaindorf.demo_2023.Demo_2023;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Demo_2023.MODID);
+
+    public static final RegistryObject<Item> Magic_Core =
+            ITEMS.register("magic_core",
+                    () -> new Item(new Item.Properties()
+                            .stacksTo(1)
+                            .tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> Water_Core =
+            ITEMS.register("water_core",
+                    () -> new Item(new Item.Properties()
+                            .stacksTo(1)
+                            .tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> Fire_Core =
+            ITEMS.register("fire_core",
+                    () -> new Item(new Item.Properties()
+                            .stacksTo(1)
+                            .tab(CreativeModeTab.TAB_MISC)));
+
+    public static final RegistryObject<Item> Rock_Core =
+            ITEMS.register("rock_core",
+                    () -> new Item(new Item.Properties()
+                            .stacksTo(1)
+                            .tab(CreativeModeTab.TAB_MISC)));
+}
