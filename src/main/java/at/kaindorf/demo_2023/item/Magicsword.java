1561b7425bba14fbe13d108922f75048b500b2ee
Jonas brunner <94897090+brujob20@users.noreply.github.com>
2024-01-09 12:57:00 +0100

    Sword Enchantments Spawn Egg Names

--- src/main/java/at/kaindorf/demo_2023/item/Magicsword.java
+++ src/main/java/at/kaindorf/demo_2023/item/Magicsword.java
@@ -1,10 +1,19 @@
 package at.kaindorf.demo_2023.item;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class Magicsword extends SwordItem {
     public Magicsword() {
         super(new CustomToolMagic(), 10, -1.0F, new Properties().tab(CreativeModeTab.TAB_COMBAT));
+    }
+
+    // Diese Methode wird überschrieben, um das Schwert zu initialisieren
+    @Override
+    public void onCraftedBy(ItemStack stack, net.minecraft.world.level.Level world, net.minecraft.world.entity.player.Player player) {
+        // Füge Fire Aspect I hinzu
+        stack.enchant(Enchantments.SMITE, 2);
     }
 }
--- src/main/java/at/kaindorf/demo_2023/item/Watersword.java
+++ src/main/java/at/kaindorf/demo_2023/item/Watersword.java
@@ -1,13 +1,22 @@
 package at.kaindorf.demo_2023.item;
 
 import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tiers;
+import net.minecraft.world.item.enchantment.Enchantments;
 
 public class Watersword extends SwordItem {
 
     public Watersword() {
         super(new CustomToolWater(), 11, -1.4F, new Properties().tab(CreativeModeTab.TAB_COMBAT));
+    }
+
+    // Diese Methode wird überschrieben, um das Schwert zu initialisieren
+    @Override
+    public void onCraftedBy(ItemStack stack, net.minecraft.world.level.Level world, net.minecraft.world.entity.player.Player player) {
+        // Füge Fire Aspect I hinzu
+        stack.enchant(Enchantments.MOB_LOOTING, 1);
     }
 
 }
